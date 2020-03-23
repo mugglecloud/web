1d8eed4262991888729a84ffce3067a9d1253078
jiangjianyi <jiangjianyi@zhiweicloud.com>
2020-03-23 17:45:28 +0800

    .

--- packages/web-client/src/config/routes.js
+++ packages/web-client/src/config/routes.js
@@ -1,7 +1,12 @@
 import App from "pages/App/App";
 
 const routes = [
-  { path: "/", exact: true, ui_components: [{ name: "app", component: App }] }
+  {
+    path: "/",
+    exact: true,
+    need_auth: false,
+    ui_components: [{ name: "app", component: App }]
+  }
 ];
 
 export default routes;
